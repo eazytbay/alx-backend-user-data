cccc65388d9fe434ff580bab7a679ee7901dabbd
eazytbay <eazytbay@gmail.com>
2024-08-08 10:53:09 +0100

    app.py another fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,45 +12,66 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-AUTH_TYPE = os.getenv('AUTH_TYPE', None)
-if AUTH_TYPE == "auth":
+
+if getenv('AUTH_TYPE') == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+
+elif getenv('AUTH_TYPE') == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
 
 @app.before_request
-def before_req():
-    """Filters each request"""
+def filter_request():
     if auth is None:
-        pass
-    else:
-        excluded = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
-        ]
-        path = request.path
-        if auth.require_auth(path, excluded):
-            if not auth.is_authorized(request):
-                return jsonify({'error': 'Unauthorized'}), 401
-            user = auth.get_current_user(request)
-            if not user:
-                return jsonify({'error': 'Forbidden'}), 403
+        return
+
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized_request(error) -> str:
+    '''Unauthorized requests
+    '''
+
+    return jsonify({
+        "error": "Unauthorized"
+    }), 401
+
+
+@app.errorhandler(403)
+def forbidden_request(error) -> str:
+    '''Forbidden request
+    '''
+
+    return jsonify({
+        "error": "Forbidden"
+    }), 403
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.config['JSONIFY_PRETTYPRINT'] = True  # Optional for readability in development
-    app.headers['Content-Type'] = 'application/json'
     app.run(host=host, port=port)
-
